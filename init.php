2c8df72b69e561fc54aabb4b602c0b5a845d5f1c
Denetor <gfc-com@mail.ru>
2018-06-08 18:07:52 +0300

    init

---
+++ init.php
@@ -0,0 +1,15 @@
+<?php
+
+define('ROOT', dirname(__FILE__));
+define('VIEW', ROOT.'/view');
+define('CONT', ROOT.'/controller');
+define('MODEL', ROOT.'/model');
+define('OBJECT', ROOT.'/object');
+define('VENDOR', ROOT.'/vendor');
+define('SIGN', ROOT.'/sign');
+define('SKETCH', ROOT.'/vendor/sketch');
+
+require_once(VENDOR.'/AutoLoad.php');
+
+$router = new sign\SignMain();
+$router->run();
---
+++ vendor/AutoLoad.php
@@ -0,0 +1,44 @@
+<?php
+
+spl_autoload_register("AutoLoad");
+
+function AutoLoad($className)
+{
+    $path = $className;
+/*    if ($lastNsPos = strrpos($className, '\\')) {
+        $className = substr($className, $lastNsPos + 1);
+    }*/
+
+    //echo $className.'<br>';
+
+    $dirs = [
+        '',
+        '/controller',
+        '/database',
+        '/model',
+        '/object',
+        '/sign',
+        '/vendor',
+        '/vendor/sketch',
+        '/vendor/sketch/sign',
+        '/vendor/sketch/sign/model',
+    ];
+
+    $found = false;
+    foreach ($dirs as $dir) {
+        $fileName = ROOT . $dir. '/'. $path . '.php';
+        //echo $fileName.'<br>';
+        if (is_file($fileName)) {
+
+            //echo '---------------------------<br>';
+
+            require_once($fileName);
+            $found = true;
+            break;
+        }
+    }
+    if (!$found) {
+        throw new Exception('Нет файла класса для загрузки: ' . $path );
+    }
+    return true;
+}
