5632ccfcf091e94e30335c7a9a9471133c674513
Denetor <gfc-com@mail.ru>
2018-06-18 21:32:57 +0300

    Add Test front controller

---
+++ controller/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace controller;
+
+use sketch\controller\ControllerBase;
+
+class HomeController extends ControllerBase
+{
+    public function actionIndex()
+    {
+        return  $this->render("home/index.php");
+    }
+}
